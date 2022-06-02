72a316334d60f7b49e2628a8bc36bd94ad0f8fd7
slegrand <ut5svsxgc@relay.firefox.com>
2022-06-02 19:47:58 -0400

    aoMagic8: add support for switch button

--- aoMagic8/magic8/magic8.ino
+++ aoMagic8/magic8/magic8.ino
@@ -1,9 +1,32 @@
+int switchState;
+int previousState;
+int trigger;
+
 void setup() {
   // setup the LEDS
   ledSetup();
+
+  // setup the switch
+  pinMode(2, INPUT);
+
+  // read the switch
+  switchState = digitalRead(2);
+  previousState = switchState;
+  trigger = 0;
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  ledAnimation();
+  // read the switch state
+  switchState = digitalRead(2);
+
+  if (switchState != previousState) {
+    trigger = (trigger + 1) & 1;
+  }
+
+  if (trigger) {
+    ledAnimation();    
+  }
+
+  previousState = switchState;
 }
