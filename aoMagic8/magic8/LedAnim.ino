984478ee042808d5da880c4987330ebdf4da5fb7
slegrand <ut5svsxgc@relay.firefox.com>
2022-06-02 19:19:27 -0400

    aoMagic8: move animation to its own sketch

---
+++ aoMagic8/magic8/LedAnim.ino
@@ -0,0 +1,45 @@
+// play the LED animation during Magic8 "Thinking"
+
+//----- constants
+#define LED1 5
+#define LED2 6
+#define LED3 7
+#define ANIM_DELAY 1000
+
+//----- functions
+void ledSetup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+}
+
+void ledAnimation() {
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  delay(ANIM_DELAY);
+  
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, LOW);
+  delay(ANIM_DELAY);
+
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  delay(ANIM_DELAY);
+  
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  delay(ANIM_DELAY);
+  
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  delay(ANIM_DELAY);
+
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+}
--- aoMagic8/magic8/magic8.ino
+++ aoMagic8/magic8/magic8.ino
@@ -1,30 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  // setup the LEDS
+  ledSetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-  delay(1000);
-
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  delay(1000);
-
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  delay(1000);
+  ledAnimation();
 }
