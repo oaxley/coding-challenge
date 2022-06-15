c92d49ca31eab6ea4955e15bd17593872aae8016
slegrand <ut5svsxgc@relay.firefox.com>
2022-06-15 18:50:34 -0400

    aoXPlane: add FSM to be able to set initial counter via Serial Link

--- 2206-June/aoXPlane/src/rotary/rotary.ino
+++ 2206-June/aoXPlane/src/rotary/rotary.ino
@@ -27,6 +27,8 @@ int last_clk_state, curr_clk_state;
 const int MAX_BTN_INTERVAL = 50;
 unsigned long last_btn_ts = 0;
 
+// FSM state
+int fsm_state;
 
 //----- functions
 void setup() {
@@ -42,43 +44,66 @@ void setup() {
 
     // Serial monitor
     Serial.begin(9600);
+
+    // fsm in initial state
+    fsm_state = 0;
 }
 
 
 void loop() {
-    // read the current value of CLK
-    curr_clk_state = digitalRead(CLK_PIN);
 
-    // if current != last, then know has been turned.
-    // only consider knob when CLK goes 0->1 to avoid double count effect
-    if ((curr_clk_state != last_clk_state) && (curr_clk_state == 1)) {
-        
-        // we need to determine which way the knob was turned
-        if (digitalRead(DT_PIN) != curr_clk_state) {
-            // Counter Clock-Wise
-            counter = counter - 1;
-        } else {
-            // Clock-Wise
-            counter = counter + 1;
+    // wait for the initial value
+    if (fsm_state == 0) {
+        Serial.println("Waiting for initial value");
+        int done = 0;
+        while (!done) {
+          if (Serial.available() > 0) {
+              String init_value = Serial.readString();
+              counter = init_value.toInt();
+              Serial.print("Initial value set to ");
+              Serial.println(counter);
+              done = 1;
+          }
         }
-
-        Serial.print("Counter: ");
-        Serial.println(counter);
+        fsm_state = 1;
     }
 
-    // keep track of last CLK value
-    last_clk_state = curr_clk_state;
 
-    // read the switch value
-    if (digitalRead(SW_PIN) == LOW) {
-        // only consider the button value if more than 50ms has passed
-        if (millis() - last_btn_ts > MAX_BTN_INTERVAL) {
-            Serial.println("Button Pressed!");
+    if (fsm_state == 1) {
+        // read the current value of CLK
+        curr_clk_state = digitalRead(CLK_PIN);
+
+        // if current != last, then know has been turned.
+        // only consider knob when CLK goes 0->1 to avoid double count effect
+        if ((curr_clk_state != last_clk_state) && (curr_clk_state == 1)) {
+            
+            // we need to determine which way the knob was turned
+            if (digitalRead(DT_PIN) != curr_clk_state) {
+                // Counter Clock-Wise
+                counter = counter - 1;
+            } else {
+                // Clock-Wise
+                counter = counter + 1;
+            }
+
+            Serial.print("Counter: ");
+            Serial.println(counter);
         }
 
-        last_btn_ts = millis();
-    }
+        // keep track of last CLK value
+        last_clk_state = curr_clk_state;
 
-    // small delay to have smooth reading
-    delay(10);
+        // read the switch value
+        if (digitalRead(SW_PIN) == LOW) {
+            // only consider the button value if more than 50ms has passed
+            if (millis() - last_btn_ts > MAX_BTN_INTERVAL) {
+                Serial.println("Button Pressed!");
+            }
+
+            last_btn_ts = millis();
+        }
+
+        // small delay to have smooth reading
+        delay(1);
+    }
 }
