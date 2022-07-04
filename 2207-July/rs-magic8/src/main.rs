b5272025f1f4a043de7374e19e62b5313aceed80
slegrand <ut5svsxgc@relay.firefox.com>
2022-07-04 18:35:09 -0400

    rs-magic8: initial commit

---
+++ 2207-July/rs-magic8/src/main.rs
@@ -0,0 +1,57 @@
+/* -*- coding: utf-8 -*-
+ * vim: set ft=rust
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * @author	Sebastien LEGRAND
+ * @license	Apache License 2.0
+ *
+ * @brief	Magic-8 ball in Rust
+ */
+
+//----- imports
+use std::io::{self, Write};
+
+//----- globals
+
+// available responses
+static RESPONSES: [&str; 20] = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+];
+
+
+//----- functions
+fn shake_the_ball() {
+    todo!();
+}
+
+fn query_user() -> String {
+    todo!()
+}
+
+//----- main entry point
+fn main() {
+    shake_the_ball();
+}
