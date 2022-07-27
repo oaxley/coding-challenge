dc5f3756b3f791266daa2c155bcd83ef13ce82f9
slegrand <ut5svsxgc@relay.firefox.com>
2022-07-26 21:11:58 -0400

    rs-bookmark: function to create an URL

---
+++ 2207-July/rs-bookmark/src/url/mod.rs
@@ -0,0 +1,78 @@
+/* -*- coding: utf-8 -*-
+ * vim: set ft=rust
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * @author	Sebastien LEGRAND
+ * @license	Apache License 2.0
+ *
+ * @brief	Main URL module
+ */
+
+//----- imports
+use crate::database as database;
+use crate::utils as utils;
+
+//----- globals
+// add a new url
+const SQLITE_ADD_URL: &str = "
+    INSERT INTO bookmarks
+    (url, description, tags)
+    VALUES (?1, ?2, ?3)";
+
+
+//----- functions
+
+// retrieve a string from an Option<&String>
+fn get_string(option: Option<&String>) -> String {
+    if let Some(value) = option {
+        return value.to_string();
+    } else {
+        return String::from("");
+    }
+}
+
+// create a string from a vector of tags
+fn get_tags(tags: Vec<&String>) -> String {
+    let mut value = String::from("");
+    for tag in tags {
+        if value.is_empty() {
+            value = tag.to_string();
+        } else {
+            value = value + ",";
+            value = value + &tag.to_string();
+        }
+    }
+
+    value
+}
+
+// create a new URL in the database
+pub fn create(url: Option<&String>, descr: Option<&String>, tag_vector: Vec<&String>, store: Option<&String>) -> bool {
+
+    // retrieve the database name either from arguments or environment
+    let db_name: String = utils::get_store_name(store);
+    if db_name.is_empty() {
+        eprintln!("Error: unable to determine the database name!");
+        return false;
+    }
+
+    // retrive the url, description & tags
+    let url = get_string(url);
+    let description = get_string(descr);
+    let tags = get_tags(tag_vector);
+
+    // create the connection to the DB
+    match database::connect(&db_name) {
+        Some(db) => {
+            match db.execute(SQLITE_ADD_URL, (&url, &description, &tags)) {
+                Ok(_) => return true,
+                Err(_) => return false
+            }
+        },
+        None => return false
+    }
+}
