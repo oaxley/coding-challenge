4ddb2247b93e56b942085f7e2a5ef42621ccc6d9
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-03 18:25:03 -0400

    day1: add Alsa initialization functions

--- 2208-August/part1/tone.c
+++ 2208-August/part1/tone.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <alsa/asoundlib.h>
 
 
 //----- typedefs
@@ -26,7 +27,14 @@ typedef unsigned char bool;
 const bool false = 0;
 const bool true = 1;
 
-const int default_duration = 1;
+const int channels = 1;             // number of channels
+const int default_duration = 1;     // duration in seconds
+const int sampling_freq = 44100;    // sampling frequency
+const int buffer_length = 48000;    // buffer length for Alsa
+
+
+//----- globals
+snd_pcm_t* device = NULL;           // global variable >_<
 
 
 //----- functions
@@ -46,11 +54,79 @@ void help() {
  *
  * Args:
  *      messsage: the error message
+ *      errnum: the error number value from Alsa
  */
 void printError(char* message, int errnum) {
     printf("Error: %s\n", message);
+    if (errnum < 0) {
+        printf("%s\n", strerror(errnum));
+    }
 }
 
+/* open Alsa backend
+ *
+ * Args:
+ *      name: the alsa device name (optional)
+ *
+ * Returns:
+ *      true if the backend has been correctly opened, false otherwise
+ */
+bool openAlsa(char* name) {
+    int err;
+
+    // set the name by default if none has been provided by the user
+    if (name == NULL) {
+        name = "default";
+    }
+
+    // open the device
+    if ((err = snd_pcm_open(&device, name, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+        printError("cannot open audio device.", err);
+        return false;
+    }
+
+    return true;
+}
+
+/* close Alsa backend */
+bool closeAlsa() {
+    int err;
+
+    // drain the audio interface
+    if ((err = snd_pcm_drain(device)) < 0) {
+        printError("unable to drain the audio interface.", err);
+        return false;
+    }
+
+    // close the audio device
+    if ((err = snd_pcm_close(device)) < 0) {
+        printError("unable to close the audio device.", err);
+        return false;
+    }
+
+    return true;
+}
+
+/* setup Alsa parameters */
+bool setupAlsa() {
+    int err;
+
+    err = snd_pcm_set_params(device,
+                             SND_PCM_FORMAT_FLOAT,
+                             SND_PCM_ACCESS_RW_INTERLEAVED,
+                             channels,
+                             sampling_freq,
+                             1,
+                             500000
+    );
+
+    if (err < 0) {
+        printError("unable to set interface parameters.", err);
+        return false;
+    }
+
+    return true;
+}
 
 //----- main entry point
 int main(int argc, char* argv[]) {
@@ -58,8 +134,7 @@ int main(int argc, char* argv[]) {
     int frequency = 0;
     int duration = default_duration;
 
-    // check for value on the command line
-
+    //--- check for values on the command line
     // no arguments
     if (argc == 1) {
         help();
@@ -83,6 +158,24 @@ int main(int argc, char* argv[]) {
         printError("unable to convert duration value.", 0);
         exit(1);
     }
+    //--- end
+
+
+    // print alsa version
+    printf("ALSA library version: %s\n", SND_LIB_VERSION_STR);
+
+    // open alsa device
+    if (!openAlsa(NULL)) {
+        return 1;
+    }
+
+    // set the interface parameters
+    if (!setupAlsa()) {
+        return 1;
+    }
+
+    // drain and close
+    closeAlsa();
 
     return 0;
 }
