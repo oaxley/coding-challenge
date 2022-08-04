70d2c4bd05b3b16d3e30b21f9fb91ba8e480f736
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-04 18:03:49 -0400

    day2: create generics header

---
+++ 2208-August/part2/generics.h
@@ -0,0 +1,28 @@
+/* -*- coding: utf-8 -*-
+ * vim: set ft=c
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * @author	Sebastien LEGRAND
+ * @license	Apache License 2.0
+ *
+ * @brief	Generics / Typedefs definition
+ */
+
+//----- guards
+#ifndef H_GENERICS
+#define H_GENERICS
+
+//----- typedefs
+typedef unsigned char bool;
+
+
+//----- constants
+const bool false = 0;
+const bool true = 1;
+
+
+#endif // H_GENERICS
