73e45f7ddd02c00b2e3e071312d1b1cdefda7b3b
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-11 08:36:52 -0400

    day5: initialize format plugins directory

---
+++ 2208-August/day5/formats/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# vim: filetype=python
+#
+# This source file is subject to the Apache License 2.0
+# that is bundled with this package in the file LICENSE.txt.
+# It is also available through the Internet at this address:
+# https://opensource.org/licenses/Apache-2.0
+#
+# @author	Sebastien LEGRAND
+# @license	Apache License 2.0
+#
+# @brief	Plugins directory to support multiple formats
+
+#----- imports
+from importlib import (
+    import_module, resources
+)
+
+
+#----- begin
+# import all the plugins in this directory
+for name in resources.contents(__name__):
+
+    # bypass special files
+    if name.startswith("__"):
+        continue
+
+    # import other python files
+    if name.endswith(".py"):
+        import_module(f"{__name__}.{name[:-3]}")
