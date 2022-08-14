f152f60e070608ee507e48aff6954c6dd92e6ab2
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-14 14:17:05 -0400

    day5: add MOD sample dataclass

--- 2208-August/day5/definitions/info.py
+++ 2208-August/day5/definitions/info.py
@@ -13,14 +13,35 @@
 
 #----- imports
 from __future__ import annotations
+from typing import List
+
+from dataclasses import dataclass
 
 
 #----- classes
+@dataclass
+class ModSample:
+    """Single MOD sample information"""
+    name: str
+    length: int
+    fine_tune: int
+    volume: int
+    loop_start: int
+    loop_length: int
+
+
 class ModInformation:
     """Aggregate all the MOD information in one place"""
 
     def __init__(self) -> None:
         """Constructor"""
+        self.file_fmt: str = ""
+        self.song_name: str = ""
+        self.channels: int = 0
+        self.instruments: List[ModSample] = []
+        self.song_length: int = 0
+        self.order: List[int] = []
+        self.pattern: int = 0
 
     def display(self) -> None:
         """Display the information"""
