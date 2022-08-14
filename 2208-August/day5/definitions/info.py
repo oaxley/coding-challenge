b6c627c0893a6beff514a6b680dd48013d05ddd0
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-14 15:44:20 -0400

    day5: create display mod information

--- 2208-August/day5/definitions/info.py
+++ 2208-August/day5/definitions/info.py
@@ -40,9 +40,28 @@ class ModInformation:
         self.channels: int = 0
         self.instruments: List[ModSample] = []
         self.song_length: int = 0
-        self.order: List[int] = []
-        self.pattern: int = 0
+        self.tracks: List[int] = []
+        self.max_pattern: int = 0
 
     def display(self) -> None:
         """Display the information"""
+        print("====== Music MOD Information ======")
+        print(f"Format      : {self.file_fmt}")
+        print(f"Song Name   : {self.song_name}")
+        print(f"Song Length : {self.song_length} tracks")
+        print(f"Channels    : {self.channels}")
+        print(f"Max Pattern : {self.max_pattern}")
+        print(f"Track List  : {self.tracks[:self.song_length]}")
 
+        print(f"Instruments :")
+        print(f"    | {' ':^22} | {'Length':^10} | {' ':^8} | {' ':^8} | {'Loop':^7} | {'Loop':^7} |")
+        print(f" ID | {'Name':^22} | {'(bytes)':^10} | {'Finetune':^8} | {'Volume':^8} | {'Start':^7} | {'Length':^7} |")
+        print(f"----+{'-' * 24}+{'-' * 12}+{'-' * 10}+{'-' * 10}+{'-' * 9}+{'-' * 9}+")
+        for i, sample in enumerate(self.instruments):
+            # don't output empty instruments
+            if sample.length == 0:
+                continue
+
+            print(f" {i:02d} | {sample.name:>22} | {sample.length:>10} | {sample.fine_tune:>8} | {sample.volume:>8} | {sample.loop_start:>7} | {sample.loop_length:>7} |")
+
+        print("")
