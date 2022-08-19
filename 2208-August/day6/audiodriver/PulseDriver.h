d639f49814d38cd3647c98088e7290e7436629e0
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-19 18:48:44 -0400

    day6: add Pulse audio driver

---
+++ 2208-August/day6/audiodriver/PulseDriver.h
@@ -0,0 +1,48 @@
+/* -*- coding: utf-8 -*-
+ * vim: set ft=cpp
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * @author	Sebastien LEGRAND
+ * @license	Apache License 2.0
+ *
+ * @brief	PulseAudio Driver
+ */
+
+//----- guards
+#ifndef PULSE_DRIVER_H
+#define PULSE_DRIVER_H
+
+
+//----- includes
+#include <memory>
+
+#include "../slMacros.h"
+#include "idriver.h"
+
+
+//----- class
+BEGIN_NAMESPACE(Audio)
+
+class PulseDriver : public IDriver
+{
+    public:     // public methods
+        PulseDriver();
+        virtual ~PulseDriver();
+
+        virtual void open(Parameters* pParams);
+        virtual void close();
+        virtual void setup();
+        virtual void push(float* buffer, int frames, int count);
+
+    private:    // private members
+        struct OpaqueData;
+        std::unique_ptr<OpaqueData> data_;
+};
+
+END_NAMESPACE(Audio)
+
+#endif // PULSE_DRIVER_H
