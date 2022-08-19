38f2612fc4a10370f33c22aece23d91e8358b42e
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-19 18:32:01 -0400

    day6: add Macros definition

---
+++ 2208-August/day6/slMacros.h
@@ -0,0 +1,17 @@
+/* -*- coding: utf-8 -*-
+ * vim: filetype=cpp
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * @author	Sebastien LEGRAND
+ * @license	Apache License 2.0
+ *
+ * @brief	Macros definitions
+ */
+
+// namespace declarations
+#define BEGIN_NAMESPACE(name)  namespace name {
+#define END_NAMESPACE(name) };
