fc66b1dc7da2cb24cc3bece70a17d922e19ffe28
slegrand <ut5svsxgc@relay.firefox.com>
2022-08-19 20:50:34 -0400

    day6: add AudioDriver enum

---
+++ 2208-August/day6/Audio/Audio.h
@@ -0,0 +1,31 @@
+/* -*- coding: utf-8 -*-
+ * vim: filetype=cpp
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the Internet at this address:
+ * https://opensource.org/licenses/Apache-2.0
+ *
+ * @author	Sebastien LEGRAND
+ * @license	Apache License 2.0
+ *
+ * @brief	Main header file for Audio namespace
+ */
+
+//----- includes
+#include "../slMacros.h"
+
+#include "idriver.h"
+#include "except.h"
+#include "AlsaDriver.h"
+#include "PulseDriver.h"
+
+
+BEGIN_NAMESPACE(Audio)
+
+typedef enum {
+    ALSA_DRIVER,
+    PULSE_DRIVER
+} DriverEnum;
+
+END_NAMESPACE(Audio)
